71cd3779137499fc1677f954d85cfdb71649af64
Shashvat Upadhyay <shashvatu56@gmail.com>
2025-09-26 01:33:58 +0530

    add home page

--- fstack/fstack/urls.py
+++ fstack/fstack/urls.py
@@ -20,7 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 
+from . import views
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
--- fstack/tweet/views.py
+++ fstack/tweet/views.py
@@ -12,6 +12,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+
 @login_required
 def tweet_create(request):
     if request.method == 'POST':
